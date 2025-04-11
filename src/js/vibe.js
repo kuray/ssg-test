c5304a77dcd8c252107247a72b2d19a9b4eec2ad
Kuntola Ray <6662953+kuray@users.noreply.github.com>
2025-04-11 17:12:12 -0400

    Commit 04/11/2025

--- src/js/vibe.js
+++ src/js/vibe.js
@@ -9,3 +9,18 @@ var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
 
 //add vibe to page
 document.querySelector(".vibe").append(vibe);
+
+
+//JS Function Practice
+
+function add(a,b){
+    return a + b;
+}
+
+console.log(add(2,3));
+
+const add2 = function(a, b){
+    return a+b
+}
+
+console.log (add(5,6));
